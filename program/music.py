07bb9e454ab12879053e292ee23044fd005a4036
SakirBey1 <sakirhack81@gmail.com>
2022-04-04 02:11:00 +0300

    Developer @SakirBey1

--- program/music.py
+++ program/music.py
@@ -38,7 +38,7 @@ async def ytdl(format: str, link: str):
     return 0, stderr
 
 
-@Client.on_message(command(["oynat", f"oynat@{BOT_USERNAME}"]) & other_filters)
+@Client.on_message(command(["oynat", f"play@{BOT_USERNAME}"]) & other_filters) #Developer SakirBey
 async def play(c: Client, m: Message):
     await m.delete()
     replied = m.reply_to_message
