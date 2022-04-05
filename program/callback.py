8abd4576b17195a5c3f9d98db4739e49adf46e61
Mehmetjsj <96945850+Mehmetjsj@users.noreply.github.com>
2022-04-05 20:34:55 +0300

    update callback.py

--- program/callback.py
+++ program/callback.py
@@ -101,7 +101,11 @@ async def cbcmds(_, query: CallbackQuery):
 @Client.on_callback_query(filters.regex("cbbasic"))
 async def cbbasic(_, query: CallbackQuery):
     await query.edit_message_text(
-       f"""🏮 Basit komutlar listesi: \n\n» /oynat istediğiniz şarkıyı direk bulup oynatır \n» /voynat isteidğin videoyu direk bulup oynatır \n» /atla sıradaki şarkıya geçer \n» /video videoyu bulup indirir \n» /indir müziği bulup indirir \n» /devam duraklatığınız şarkıyı devam ettirir \n» /durdur akışı durdurur \n» /bitir akışı bitirip sesli sohbetten ayrılır \n» /canlı canlı yayın akışını sağlar \n\n⚡️ Daha fazla bilgi icin @botsohbetdestek""",
+       f"""🏮 Basit komutlar listesi:
+         
+    /oynat istediğiniz şarkıyı direk bulup oynatır \n» /voynat isteidğin videoyu direk bulup oynatır \n» /atla sıradaki şarkıya geçer \n» /video videoyu bulup indirir \n» /indir müziği bulup indirir \n» /devam duraklatığınız şarkıyı devam ettirir \n» /durdur akışı durdurur \n» /bitir akışı bitirip sesli sohbetten ayrılır \n» /canlı canlı yayın akışını sağlar \n\n⚡️ Daha fazla bilgi icin
+    
+      @botsohbetdestek""",
 
         reply_markup=InlineKeyboardMarkup(
             [
