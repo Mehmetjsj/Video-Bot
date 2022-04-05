6340f38528928da12c1137469693d1d9bda12106
Mehmetjsj <96945850+Mehmetjsj@users.noreply.github.com>
2022-04-05 03:51:08 +0300

    update video.py

--- program/video.py
+++ program/video.py
@@ -55,7 +55,7 @@ async def ytdl(link):
         return 0, stderr.decode()
 
 
-@Client.on_message(command(["vplay", f"voynat@{BOT_USERNAME}"]) & other_filters)
+@Client.on_message(command(["voynat", f"vplay@{BOT_USERNAME}"]) & other_filters)
 async def vplay(c: Client, m: Message):
     await m.delete()
     replied = m.reply_to_message
