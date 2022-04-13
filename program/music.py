852e3464af9789debd5b1db0335d0fa983489fda
aylak-github <yk3514003@gmail.com>
2022-04-13 22:18:09 +0000

    🧩 Otomatik Düzeltme
    Signed-off-by: aylak-github <yk3514003@gmail.com>

--- program/music.py
+++ program/music.py
@@ -38,9 +38,7 @@ async def ytdl(format: str, link: str):
     return 0, stderr
 
 
-@Client.on_message(
-    command(["oynat", f"play@{BOT_USERNAME}"]) & other_filters
-) 
+@Client.on_message(command(["oynat", f"play@{BOT_USERNAME}"]) & other_filters)
 async def play(c: Client, m: Message):
     await m.delete()
     replied = m.reply_to_message
