a18ecdf1b4e81014f4d1b5c068a4d7c16802b6d7
Mehmetjsj <96945850+Mehmetjsj@users.noreply.github.com>
2022-04-18 19:26:01 +0300

    update updater.py

--- program/updater.py
+++ program/updater.py
@@ -71,8 +71,8 @@ async def update_repo(_, message: Message):
 @Client.on_message(command(["restart", f"restart@{BOT_USERNAME}"]) & ~filters.edited)
 @sudo_users_only
 async def restart_bot(_, message: Message):
-    msg = await message.reply("`restarting bot...`")
+    msg = await message.reply("`Bot yeniden Başlatılıyor...`")
     args = [sys.executable, "main.py"]
-    await msg.edit("✅ bot restarted\n\n• now you can use this bot again.")
+    await msg.edit("✅ bot yeniden başlatıldı\n\n• keyifli kullanımlar.")
     execle(sys.executable, *args, environ)
     return
