cb820f2fe5959d910dc046e5e123351fa9d9d59e
aylak-github <yk3514003@gmail.com>
2022-05-07 13:39:19 +0000

    🧩 Otomatik Düzeltme
    Signed-off-by: aylak-github <yk3514003@gmail.com>

--- driver/filters.py
+++ driver/filters.py
@@ -1,14 +1,11 @@
-from pyrogram import filters
 from typing import List, Union
+
+from pyrogram import filters
+
 from config import cmd_handler
 
-
-filters_ex = (
-    filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
-)
-filters_in = (
-    filters.private & ~filters.edited & ~filters.via_bot & ~filters.forwarded
-)
+filters_ex = filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
+filters_in = filters.private & ~filters.edited & ~filters.via_bot & ~filters.forwarded
 
 
 def command(commands: Union[str, List[str]]):
