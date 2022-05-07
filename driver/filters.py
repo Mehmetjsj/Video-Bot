438c3bb846d4b92a1a462743abb70df5c93b446b
Mehmetjsj <96945850+Mehmetjsj@users.noreply.github.com>
2022-05-07 18:33:18 +0300

    update filters.py

--- driver/filters.py
+++ driver/filters.py
@@ -4,7 +4,9 @@ from pyrogram import filters
 
 from config import cmd_handler
 
-filters_ex = filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
+filters_ex = (
+    filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
+)
 filters_in = filters.private & ~filters.edited & ~filters.via_bot & ~filters.forwarded
 
 
