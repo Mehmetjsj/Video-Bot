bf2ac50f174f5b1e50d06c5409ba3abc5a26aa08
aylak-github <yk3514003@gmail.com>
2022-07-07 17:14:47 +0000

    🧩 Otomatik Düzeltme
    Signed-off-by: aylak-github <yk3514003@gmail.com>

--- driver/filters.py
+++ driver/filters.py
@@ -1,7 +1,8 @@
-from pyrogram import filters
 from typing import List, Union
-from config import COMMAND_PREFIXES
 
+from pyrogram import filters
+
+from config import COMMAND_PREFIXES
 
 other_filters = filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
 other_filters2 = (
@@ -11,4 +12,6 @@ other_filters2 = (
 
 def command(commands: Union[str, List[str]]):
     return filters.command(commands, COMMAND_PREFIXES)
+
+
 Footer
